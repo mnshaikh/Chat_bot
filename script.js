cfc180bb76c0215cb0fa00d6e287f7fbcd4b938e
mnshaikh <33232592+mnshaikh@users.noreply.github.com>
2018-05-31 20:31:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+var data;
+
+function f_key(e){
+   if (e.keyCode === 13){
+       f_send();
+   }
+}
+
+
+function f_send() {
+    var odiv = document.createElement('div'); 
+    odiv.className = 'c_out';
+    var text = document.getElementById('i_text_box').value;
+    var omsg = document.createTextNode(text);
+    odiv.appendChild(omsg);
+    var main = document.getElementById('i_main');
+    if (text == "") {
+    }
+    else {
+        main.appendChild(odiv);
+    document.getElementById('i_text_box').value = "";
+        f_fetch();
+    }
+      
+     main.scrollTop = main.scrollHeight;    
+}
+
+function f_fetch() {
+    if (data == undefined) {
+    var url = "http://samples.openweathermap.org/data/2.5/weather?q=India&appid=b6907d289e10d714a6e88b30761fae22";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            data = JSON.parse(xmlhttp.responseText);
+            f_receive();
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(); 
+        }
+}
+
+function f_receive() {
+    var odiv = document.createElement('div'); 
+    odiv.className = 'c_in';
+    var text = data.main.temp;
+    var omsg = document.createTextNode(text);
+    odiv.appendChild(omsg);
+    var main = document.getElementById('i_main');
+    if (text == "") {
+    }
+    else {
+        main.appendChild(odiv);
+    data = undefined;
+    }      
+     main.scrollTop = main.scrollHeight;  
+}
